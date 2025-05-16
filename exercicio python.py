b2204e95cd11c1a4d185bb23faaa2ed8b93801fb
CJR <4545854140@estudante.sed.sc.gov.br>
2025-05-16 11:11:55 -0300

    Add files via upload

---
+++ exercicio python.py
@@ -0,0 +1,134 @@
+# 3- peça ao usuário uma palavra e mostre quantas vogais ela tem
+
+palavra = input('Digite uma palavra: ')
+contadorvogais = 0
+
+
+for letra in palavra:
+    if letra == 'a' or 'e' or 'i' or 'o' or 'u':
+        contadorvogais += 1
+
+
+print('o numero de vogais é ' , contadorvogais)
+
+
+# 4- peça ao usuário para digitar 6 números e mostre apenas os números pares digitados
+
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite um numero: '))
+numero3 = int(input('Digite um numero: '))
+numero4 = int(input('Digite um numero: '))
+numero5 = int(input('Digite um numero: '))
+numero6 = int(input('Digite um numero: '))
+
+
+if numero1 % 2 == 0:
+    print('o numero' , numero1 , 'é par')
+
+
+else:
+        print(numero1 , 'Não é par')
+
+
+if numero2 % 2 == 0:
+    print('o numero' , numero2 ,  'é par')
+
+
+else:
+        print(numero2 , 'Não é par')
+
+
+if numero3 % 2 == 0:
+    print('o numero' , numero3 ,  ' é par')
+
+
+else:
+        print(numero3 , 'Não é par')
+
+
+if numero4 % 2 == 0:
+    print('o numero' , numero4 ,  ' é par')
+
+
+else:
+        print(numero4 , 'Não é par')
+
+
+if numero5 % 2 == 0:
+    print('o numero' , numero5 ,  ' é par')
+
+
+else:
+        print(numero5 , 'Não é par')
+
+
+if numero6 % 2 == 0:
+    print('o numero' , numero6 ,  ' é par')
+
+
+else:
+        print(numero6 , 'Não é par')
+
+
+
+
+
+# 5-Solicite as notas de 4 provas e mostre a média.
+
+nota1 = float(input('Digite sua nota'))
+nota2 = float(input('Digite sua nota'))
+nota3 = float(input('Digite sua nota'))
+nota4 = float(input('Digite sua nota'))
+
+
+soma = nota1 + nota2 + nota3 + nota4
+
+
+media = soma / 4
+print('sua media e' , media)
+
+
+# 6- Peça ao usuário um número e mostre a tabuada desse número de 1 a 10.
+
+numero = int(input('Digite um numero: '))
+contadorwhile = 1
+
+
+while contadorwhile <= 10:
+    print(numero , 'x' , contadorwhile , '=' , numero * contadorwhile)
+    contadorwhile += 1
+
+#7 - Peça um número N ao usuário e mostre todos os números de 1 até N.
+
+numero = int(input('Digite um numero: '))
+contador = 1
+
+
+while contador <= numero:
+    print(contador)
+    contador += 1
+
+
+#8 - Peça ao usuário uma palavra e mostre ela ao contrário.
+
+palavra = input('Digite uma palavra: ')
+palavracontrario = palavra[::-1]
+
+
+print(palavracontrario)
+
+
+#9 - Peça um número ao usuário e diga se ele é múltiplo de 3.
+
+numero = int(input('Digite um numero: '))
+
+
+if numero % 3 == 0:
+    print('É multiplo de 3')
+else:
+    print('não é multiplo de 3')
+
+
+
+#10 - Peça ao usuário para digitar 3 nomes e mostre todos eles em ordem alfabética.
+
